699486ee4f8a1df100e4d2ec3d40db163048e491
Suryana <nanasuryana335@gmail.com>
2024-06-30 03:56:12 +0700

    edit: modify invoices table

--- server/repository/migrations/20240629203228_create_invoices_table.js
+++ server/repository/migrations/20240629203228_create_invoices_table.js
@@ -5,19 +5,18 @@
 exports.up = function (knex) {
     return knex.schema.createTable("invoices", function (table) {
       table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()"));
-      table.string("invoice_number").defaultTo(knex.raw("gen_random_uuid")).notNullable();
+      table.string("invoice_number").defaultTo(knex.raw("gen_random_uuid()")).notNullable();
       table.date("issue_date").defaultTo(knex.fn.now());
-      table.date("due_date").defaultTo(knex.raw("CURRENT_DATE + INTERVAL '1 day'"));
+      table.date("due_date").defaultTo(knex.raw("CURRENT_DATE + INTERVAL '1 day'")); // Corrected default expression
       table.decimal("total_amount", 10, 2).notNullable();
       table.enu("status", ["paid", "unpaid"]).notNullable();
-
       table.uuid("order_id").nullable();
       table.uuid("payment_method_id").nullable();
       table.timestamps(true, true);
   
       // Foreign key constraints
       table.foreign("order_id").references("id").inTable("orders").onDelete("CASCADE");
-      table.foreign("payment_method").references("id").inTable("payment_methods").onDelete("CASCADE");
+      table.foreign("payment_method_id").references("id").inTable("payment_methods").onDelete("CASCADE");
     });
   };
 

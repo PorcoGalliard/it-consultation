21bb51220b1c95e8e47b823260df252972b39642
Suryana <nanasuryana335@gmail.com>
2024-06-30 02:41:31 +0700

    edit: modify consultants field structure

--- server/repository/migrations/20240629193214_create_consultants_table.js
+++ server/repository/migrations/20240629193214_create_consultants_table.js
@@ -3,26 +3,26 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-    return knex.schema.createTable("consultants", function (table) {
-      table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()"));
-      table.string("nama_brand").notNullable();
-      table.string("username").notNullable();
-      table.string("email").unique().notNullable();
-      table.string("password").notNullable();
-      table.string("nomor_telepon");
-      table.text("picture");
-      table.enu("gender", ["male", "female"]).notNullable();
-      table.string("alamat");
-      table.date("tanggal_lahir");
-      table.boolean("is_admin").defaultTo(false);
-      table.timestamps(true, true);
-    });
-  };
+  return knex.schema.createTable("consultants", function (table) {
+    table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()"));
+    table.string("nama_brand").notNullable();
+    table.string("username").notNullable();
+    table.string("email").unique().notNullable();
+    table.string("password").notNullable();
+    table.text("bio");
+    table.string("nomor_telepon");
+    table.string("NIK").notNullable();
+    table.text("picture");
+    table.enu("gender", ["male", "female"]).notNullable();
+    table.string("alamat");
+    table.date("tanggal_lahir");
+    table.boolean("is_admin").defaultTo(false);
+    table.timestamps(true, true);
+  });
+};
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};

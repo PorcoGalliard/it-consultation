3b504fb689df7f8c077eb5f00b7b74426ddd5b4d
Suryana <nanasuryana335@gmail.com>
2024-06-30 03:47:46 +0700

    feat: create invoices schema

---
+++ server/repository/migrations/20240629203228_create_invoices_table.js
@@ -0,0 +1,30 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable("orders", function (table) {
+      table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()"));
+      table.string("invoice_number").defaultTo(knex.raw("gen_random_uuid")).notNullable();
+      table.date("issue_date").defaultTo(knex.fn.now());
+      table.date("due_date").defaultTo(knex.raw("CURRENT_DATE + INTERVAL '1 day'"));
+      table.decimal("total_amount", 10, 2).notNullable();
+      table.enu("status", ["paid", "unpaid"]).notNullable();
+
+      table.uuid("order_id").nullable();
+      table.uuid("payment_method_id").nullable();
+      table.timestamps(true, true);
+  
+      // Foreign key constraints
+      table.foreign("order_id").references("id").inTable("orders").onDelete("CASCADE");
+      table.foreign("payment_method").references("id").inTable("payment_methods").onDelete("CASCADE");
+    });
+  };
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};

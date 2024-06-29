c02aeedf6fea0669892e38ba44a91b8a780285df
Suryana <nanasuryana335@gmail.com>
2024-06-30 03:28:44 +0700

    feat: create orders schema

---
+++ server/repository/migrations/20240629201617_create_orders_table.js
@@ -0,0 +1,29 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable("orders", function (table) {
+      table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()"));
+      table.string("total_biaya").notNullable();
+      table.string("rating").nullable();
+      table.text("review").nullable();
+      table.uuid("user_id").nullable();
+      table.uuid("consultant_id").nullable();
+      table.uuid("service_id").nullable();
+      table.timestamps(true, true);
+  
+      // Foreign key constraints
+      table.foreign("user_id").references("id").inTable("users").onDelete("CASCADE");
+      table.foreign("consultant_id").references("id").inTable("consultants").onDelete("CASCADE");
+      table.foreign("service_id").references("id").inTable("payment_methods").onDelete("CASCADE");
+    });
+  };
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable("orders");
+};

71cea80c6a712d9c6d9d6f93f84beac08d01a3b9
Suryana <nanasuryana335@gmail.com>
2024-07-01 01:09:08 +0700

    edit: modify returned message when status code is 401/403

--- server/middlewares/authenticateToken.js
+++ server/middlewares/authenticateToken.js
@@ -1,11 +1,15 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
-  const token = req.headers['authorization'];
-  if (!token) return res.sendStatus(401);
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized: Missing token" });
+  }
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) {
+      return res.status(403).json({ message: "Forbidden: Invalid token" });
+    }
     req.user = user;
     next();
   });
